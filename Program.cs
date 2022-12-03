3dfea1cdf1c4d67f79924229a8b96496215692cc
Erick Jantsch <erickjantsch@gmail.com>
2022-12-03 00:25:39 +0000

    Primeira licao de C#

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace ModuloGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Atividade:
+             * Crie um programa que receba dois valores e calcule a soma desses valores.
+             * Ao final exiba o rtesultado no console.
+             */
+
+
+            #region Base
+
+
+            Console.WriteLine("Digite 1 para iniciar o programa ou digite  para sair");
+            int opcao = int.Parse(Console.ReadLine());
+            while (opcao != 0)
+            {
+                int a, b;
+                Console.WriteLine("Informe o valor dos valores dos numeros a serem somados");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                int resultado = Soma(a, b);
+                Console.WriteLine("A soma dos valores e igual a " + resultado );
+                Console.WriteLine("Se deseja repetir ou aperte 0 para sair");
+                opcao = int.Parse(Console.ReadLine());
+            }
+            #endregion
+
+            static
+
+
+
+
+
         }
     }
 }
